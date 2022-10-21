9e1641d8956b8c0592daeb432662d63ee671c792
Cufee <vkouzin@outlook.com>
2022-10-21 08:09:15 -0400

    redundant check

--- calculations/snapshot.go
+++ calculations/snapshot.go
@@ -45,7 +45,7 @@ func AccountSnapshot(account accounts.CompleteProfile, accountAchievements stati
 	var totalBattles int32
 
 	for _, vehicle := range vehicles {
-		if (vehiclesCutoffTime != 0 && vehicle.LastBattleTime < vehiclesCutoffTime) || vehicle.Stats.Battles == 0 {
+		if (vehicle.LastBattleTime < vehiclesCutoffTime) || vehicle.Stats.Battles == 0 {
 			continue
 		}
 		wg.Add(1)
